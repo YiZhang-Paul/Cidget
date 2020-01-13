4e44db85bf5e3c4b6996e190af629abf77f34e3c
YI ZHANG <scaccterran@gmail.com>
2020-01-13 00:11:44 -0500

    CID-12 fixed ci/cd pipelines

---
+++ src/core/service/io/logger/logger.ts
@@ -0,0 +1,11 @@
+export default class Logger {
+
+    public log(a: any = '', ...b: any[]): void {
+        if (process.env.APP_ENV !== 'production' && process.env.APP_ENV !== 'testing') {
+            // eslint-disable-next-line
+            console.log(a, ...b);
+        }
+    }
+}
+
+export const logger = new Logger();
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import * as config from 'config';
 import { Request, Response } from 'express';
+
+import { logger } from './core/service/io/logger/logger';
+
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const port = config.get<any>('cidget').server.port;
 const server = express();
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
-server.listen(port, () => console.log(`listening on port ${port}.`));
+server.listen(port, () => logger.log(`listening on port ${port}.`));
 
 server.post('/', (req: Request, res: Response) => {
-    console.log(req);
+    logger.log(req);
     res.sendStatus(200);
 });
