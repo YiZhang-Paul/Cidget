561eac10e455895c490150401871a1f7a3e0ca61
YI ZHANG <scaccterran@gmail.com>
2020-01-18 19:17:54 -0500

    CID-23 show tooltip only when needed

--- src/shared/components/generic/weblink-display/weblink-display.tsx
+++ src/shared/components/generic/weblink-display/weblink-display.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop } from 'vue-property-decorator';
+import { Component, Prop, Ref } from 'vue-property-decorator';
 import * as tsx from 'vue-tsx-support';
 import { shell } from 'electron';
 
@@ -10,14 +10,22 @@ export default class WeblinkDisplay extends tsx.Component<any> {
     @Prop() public url!: string;
     @Prop({ default: 'top-start' }) public tooltipPosition!: string;
     @Prop({ default: false }) public isDarkMode!: boolean;
+    @Prop() private _showTooltip = false;
+
+    @Ref('container') public container!: HTMLElement;
+
+    public mounted(): void {
+        this._showTooltip = this.container.offsetWidth < this.container.scrollWidth;
+    }
 
     public render(): any {
         return (
-            <div class="weblink-display-container">
-                <el-tooltip disabled={false}
+            <div class="weblink-display-container" ref="container">
+                <el-tooltip disabled={!this._showTooltip}
                     placement={this.tooltipPosition}
                     effect={this.isDarkMode ? 'dark' : 'light'}
                     content={this.text}>
+
                     <a class="url" onClick={() => shell.openExternal(this.url)}>
                         {this.text}
                     </a>
