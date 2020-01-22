6728609c320d3395e5da91843f748e1f4f2a197a
YI ZHANG <scaccterran@gmail.com>
2020-01-22 12:13:26 -0500

    CID-27 removed top toolbar

--- src/background.ts
+++ src/background.ts
@@ -12,6 +12,8 @@ protocol.registerSchemesAsPrivileged([{ scheme: 'app', privileges: { secure: tru
 function createWindow() {
     // Create the browser window.
     win = new BrowserWindow({
+        x: 0,
+        y: 0,
         width: 800,
         height: 600,
         transparent: true,
