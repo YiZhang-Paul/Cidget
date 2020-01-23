52073c423225858a9ab3010ba8fca3ddae76d255
YI ZHANG <scaccterran@gmail.com>
2020-01-22 20:36:09 -0500

    CID-29 azure devops data store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,23 @@
 import Vue from 'vue';
 import Vuex, { Store } from 'vuex';
 
+import AzureDevopsStore from './azure-devops/azure-devops.store';
 import GithubStore from './github/github.store';
 
 Vue.use(Vuex);
 
+const azureDevopsStoreName = 'azureDevopsStore';
 const githubStoreName = 'githubStore';
 
 const store = new Store<any>({
     modules: {
+        [azureDevopsStoreName]: AzureDevopsStore,
         [githubStoreName]: GithubStore
     }
 });
 
 export default {
     store,
+    azureDevopsStoreName,
     githubStoreName
 };
