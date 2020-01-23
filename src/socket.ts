6b9c07c926f234e44d11fd9e452d10000969f3b6
YI ZHANG <scaccterran@gmail.com>
2020-01-22 20:38:15 -0500

    CID-29 data flow to consume azure devops webhook payload

--- src/socket.ts
+++ src/socket.ts
@@ -7,9 +7,14 @@ const socket = socketClient('http://localhost:8888');
 socket.on('connect', () => logger.log('socket connected.'));
 socket.on('disconnect', () => logger.log('socket disconnected.'));
 
-socket.on('github-push', (json: any) => {
+socket.on('azure-devops-pipeline', (payload: any) => {
+    const action = `${Store.azureDevopsStoreName}/addCiBuild`;
+    Store.store.dispatch(action, payload);
+});
+
+socket.on('github-push', (data: any) => {
     const action = `${Store.githubStoreName}/addCommit`;
-    const payload = JSON.parse(json.payload);
+    const payload = JSON.parse(data.payload);
     Store.store.dispatch(action, payload);
 });
 
