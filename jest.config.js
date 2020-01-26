53fd26ae1edaa07bb49514619eed1a0ebdd615d9
YI ZHANG <scaccterran@gmail.com>
2020-01-26 15:52:56 -0500

    CID-33 setup jest mocks

--- jest.config.js
+++ jest.config.js
@@ -15,8 +15,12 @@ module.exports = {
         '!src/store/index.ts'
     ],
     moduleNameMapper: {
-        'azure-devops-node-api': '<rootDir>/src/mocks/azure-devops-node-api.ts',
-        'axios': '<rootDir>/src/mocks/axios.ts',
-        'electron': '<rootDir>/src/mocks/electron.ts'
+        'azure-devops-node-api': '<rootDir>/src/mocks/third-party/azure-devops-node-api.ts',
+        'axios': '<rootDir>/src/mocks/third-party/axios.ts',
+        'electron': '<rootDir>/src/mocks/third-party/electron.ts',
+        './features/azure-devops/build-pipeline-card/build-pipeline-card': '<rootDir>/src/mocks/components/build-pipeline-card.tsx',
+        './features/azure-devops/release-pipeline-card/release-pipeline-card': '<rootDir>/src/mocks/components/release-pipeline-card.tsx',
+        './features/github/commit-card/commit-card': '<rootDir>/src/mocks/components/commit-card.tsx',
+        './features/github/pull-request-card/pull-request-card': '<rootDir>/src/mocks/components/pull-request-card.tsx'
     }
 };
---
+++ src/mocks/components/build-pipeline-card.tsx
@@ -0,0 +1,10 @@
+import { Component } from 'vue-property-decorator';
+import * as tsx from 'vue-tsx-support';
+
+@Component
+export default class BuildPipelineCard extends tsx.Component<any> {
+
+    public render(): any {
+        return (<div></div>);
+    }
+}
