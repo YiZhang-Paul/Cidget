db0476fbe056bbd9f4cf3c93beffcebe8f3d8298
YI ZHANG <scaccterran@gmail.com>
2020-02-09 02:25:39 -0500

    CID-69 refactored outlook auth server

--- src/outlook-auth.ts
+++ src/outlook-auth.ts
@@ -1,15 +1,18 @@
+import config from 'config';
 import express from 'express';
 import { remote } from 'electron';
 
 import Types from './core/ioc/types';
 import Container from './core/ioc/container';
 import IOAuthProvider from './core/interface/general/oauth-provider.interface';
+import { logger } from './core/service/io/logger/logger';
 
 const app = express();
+const { localPort } = config.get<any>('mail').outlook;
 const outlookApi = Container.get<IOAuthProvider>(Types.IOAuthProvider);
 const window = new remote.BrowserWindow({ width: 800, height: 600 });
 
-app.listen(8889);
+app.listen(localPort, () => logger.log(`outlook OAuth server listening on port ${localPort}.`));
 window.loadURL(outlookApi.authorizeUrl);
 
 app.get('/auth/outlook', async (req, res) => {
