7c34e4b3397f2c967232b755c52f6fe7b51427d0
YI ZHANG <scaccterran@gmail.com>
2020-02-17 01:31:30 -0500

    CID-84 inject app settings in azure devops api provider

--- src/core/service/pipeline/azure-devops/azure-devops-api-provider/azure-devops-api-provider.service.ts
+++ src/core/service/pipeline/azure-devops/azure-devops-api-provider/azure-devops-api-provider.service.ts
@@ -1,15 +1,19 @@
-import { injectable } from 'inversify';
+import { injectable, inject } from 'inversify';
 import { getPersonalAccessTokenHandler, WebApi } from 'azure-devops-node-api';
 import { IBuildApi } from 'azure-devops-node-api/BuildApi';
 import { IReleaseApi } from 'azure-devops-node-api/ReleaseApi';
 
-import config from '../../../../../electron-config';
-
-const { url, token } = config.get('cicd.azureDevops');
+import Types from '../../../../ioc/types';
+import AppSettings from '../../../io/app-settings/app-settings';
 
 @injectable()
 export default class AzureDevopsApiProvider {
-    private _azureApi = new WebApi(url, getPersonalAccessTokenHandler(token));
+    private _azureApi: WebApi;
+
+    constructor(@inject(Types.AppSettings) settings: AppSettings) {
+        const { url, token } = settings.get('cicd.azureDevops');
+        this._azureApi = new WebApi(url, getPersonalAccessTokenHandler(token));
+    }
 
     public async getBuildApi(): Promise<IBuildApi> {
         return await this._azureApi.getBuildApi();
