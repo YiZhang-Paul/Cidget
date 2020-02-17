b5c200b83b96e6a1de493d883f188bf23a65d871
YI ZHANG <scaccterran@gmail.com>
2020-02-17 01:37:13 -0500

    CID-84 inject app settings in github webhook provider

--- src/core/service/webhook/github/github-webhook-provider.service.ts
+++ src/core/service/webhook/github/github-webhook-provider.service.ts
@@ -1,29 +1,37 @@
 import { injectable, inject } from 'inversify';
 
-import config from '../../../../electron-config';
 import Types from '../../../ioc/types';
 import IHttpClient from '../../../interface/general/http-client.interface';
 import IWebhook from '../../../interface/webhook/webhook.interface';
 import IWebhookQuery from '../../../interface/webhook/webhook-query.interface';
 import IWebhookProvider from '../../../interface/webhook/webhook-provider.interface';
 import IGithubWebhookContext from '../../../interface/webhook/github/github-webhook-context.interface';
-
-const { url, token, user } = config.get('repository.github');
+import AppSettings from '../../io/app-settings/app-settings';
 
 @injectable()
 export default class GithubWebhookProviderService implements IWebhookProvider<IWebhookQuery, IGithubWebhookContext> {
+    private _url: string;
+    private _token: string;
+    private _user: string;
     private _httpClient: IHttpClient;
 
-    constructor(@inject(Types.IHttpClient) httpClient: IHttpClient) {
+    constructor(
+        @inject(Types.IHttpClient) httpClient: IHttpClient,
+        @inject(Types.AppSettings) settings: AppSettings
+    ) {
+        const { url, token, user } = settings.get('repository.github');
+        this._url = url;
+        this._token = token;
+        this._user = user;
         this._httpClient = httpClient;
     }
 
     private get headers(): { [key: string]: string } {
-        return ({ Authorization: `token ${token}` });
+        return ({ Authorization: `token ${this._token}` });
     }
 
     public async listWebhooks(query: IWebhookQuery): Promise<IWebhook[]> {
-        const endpoint = `${url}/repos/${user}/${query.name}/hooks`;
+        const endpoint = `${this._url}/repos/${this._user}/${query.name}/hooks`;
         const { data } = await this._httpClient.get(endpoint, { headers: this.headers });
 
         return (data || []).map(this.toWebhook.bind(this));
@@ -52,7 +60,7 @@ export default class GithubWebhookProviderService implements IWebhookProvider<IW
             }
         };
 
-        const endpoint = `${url}/repos/${user}/${project}/hooks`;
+        const endpoint = `${this._url}/repos/${this._user}/${project}/hooks`;
         const { data } = await this._httpClient.post(endpoint, body, { headers: this.headers });
 
         return this.toWebhook(data);
